c8314eaf097d5f15708fc53f78ab7186a5aebab3
gawgaw <gawgaw.developer@gmail.com>
2021-02-10 01:32:50 +0900

    全店舗のpageをarticleに移動 店舗関係はarticle　ユーザー関係はuserに

--- wendy/pages/index.vue
+++ wendy/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>トップページ</h1>
-    <p><nuxt-link :to="`/all-articles/`">全店舗一覧</nuxt-link></p>
+    <p><nuxt-link :to="`/article/all-articles/`">全店舗一覧</nuxt-link></p>
   </div>
 </template>
 
