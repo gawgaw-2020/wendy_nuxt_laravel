dba69de5eb56683ef85691d7628a6dab458f70e4
gawgaw <gawgaw.developer@gmail.com>
2021-02-14 03:20:27 +0900

    新規登録のviewを作成

--- wendy/pages/user/registration.vue
+++ wendy/pages/user/registration.vue
@@ -1,18 +1,27 @@
 <template>
   <div>
     <h2>新規登録画面</h2>
+    <p><input type="email" v-model="email"></p>
+    <p><input type="password" v-model="password"></p>
+    <button @click="signup">新規登録</button>
   </div>
 </template>
 
 <script>
+import { mapState, mapMutations, mapActions, mapGetters } from "vuex";
+
 export default {
   data() {
     return {
-
+      email: '',
+      password: '',
     }
   },
   computed: {
     
+  },
+  methods: {
+    signup() {},
   },
   created: function() {
   },
