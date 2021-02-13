94539bb98a85f96b065b4336b4aa8e54e0efc4f6
gawgaw <gawgaw.developer@gmail.com>
2021-02-14 01:25:57 +0900

    netlifyでデプロイしたいのでfirebase.jsを修正

--- wendy/plugins/firebase.js
+++ wendy/plugins/firebase.js
@@ -4,10 +4,14 @@ import firebase from 'firebase'
 //firebaseの初期化に必要なデータをconfigで用意する
 //必要に応じてパラメーターをここで定義
 const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  // apiKey: process.env.FIREBASE_API_KEY,
+  // authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  // projectId: process.env.FIREBASE_PROJECT_ID,
+  // storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  apiKey: 'AIzaSyDJE18gz8IxkrNQdG2XvBfWTseFFWp5iek',
+  authDomain: 'wendy-nuxt-firebase.firebaseapp.com',
+  projectId: 'wendy-nuxt-firebase',
+  storageBucket: 'wendy-nuxt-firebase.appspot.com',
 }
 //二重に初期化が行われないために!firebase.app.lengthで確認をする
 if (!firebase.apps.length) {
