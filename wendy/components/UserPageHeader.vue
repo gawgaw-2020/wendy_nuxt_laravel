fdc6942b97a968cd728a708ce1414da906e87ac9
gawgaw <gawgaw.developer@gmail.com>
2021-02-14 17:06:43 +0900

    Routeからの読み込みに修正

--- wendy/components/UserPageHeader.vue
+++ wendy/components/UserPageHeader.vue
@@ -4,7 +4,7 @@
       <p class="user-icon">
         {{ userPhotoURL }}
         <img v-if="userPhotoURL" :src="userPhotoURL" alt="">
-        <img v-if="!userPhotoURL" src="img/user-icon.png" alt="">
+        <img v-if="!userPhotoURL" src="/img/user-icon.png" alt="">
       </p>
       <p>{{ userName }}様</p>
   </div>
