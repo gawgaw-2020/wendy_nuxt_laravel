d62f3c7da92f9d90615c0cf1269b6d17e0bc25f2
gawgaw <gawgaw.developer@gmail.com>
2021-02-15 19:53:25 +0900

    fontawesomeの読み込み（最終的に使うものだけ読み込む）

--- wendy/nuxt.config.js
+++ wendy/nuxt.config.js
@@ -17,7 +17,9 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.1.0/css/all.css', integrity: 'sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt', crossorigin: 'anonymous' }
+
     ]
   },
 
