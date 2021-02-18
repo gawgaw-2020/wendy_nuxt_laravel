b0d2a75d71993db88f816462dec29af81292c164
gawgaw <gawgaw.developer@gmail.com>
2021-02-18 12:49:14 +0900

    新規登録画面へのリンクボタンのスタイル作成

--- wendy/pages/login/login.vue
+++ wendy/pages/login/login.vue
@@ -1,15 +1,16 @@
 <template>
   <div>
     <LoginNavigation/>
-    <h2>ログイン画面</h2>
     <p>メールアドレス</p>
     <p><input type="email" v-model="email"></p>
     <p>パスワード</p>
     <p><input type="password" v-model="password"></p>
     <button @click="login">ログイン</button>
+    <p>はじめての方はこちら</p>
+    <p @click="setBoxPosition('registration')"  class="btn btn-additional">
+      新規登録
+    </p>
     <button @click="googleLogin">Googleアカウントでログイン</button>
-    <p>初めての方はこちら</p>
-    <nuxt-link :to="`/login/registration`">新規登録</nuxt-link>
   </div>
 </template>
 
@@ -29,6 +30,7 @@ export default {
   },
   methods: {
     ...mapActions('auth', ['googleLogin', 'firebaseLogin', 'createUser']),
+    ...mapActions("login", ["setBoxPosition"]),
     async login() {
       await this.firebaseLogin({ email: this.email, password: this.password });
     }
