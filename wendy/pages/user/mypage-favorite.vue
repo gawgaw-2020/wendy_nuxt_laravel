fd4c587c4729caa5a94435edb30623206f118ac0
gawgaw <gawgaw.developer@gmail.com>
2021-02-24 01:05:57 +0900

    DB決め打ちのお気に入りデータを表示

--- wendy/pages/user/mypage-favorite.vue
+++ wendy/pages/user/mypage-favorite.vue
@@ -3,10 +3,22 @@
     <UserPageHeader/>
     <MyPageNavigation/>
     <h2>お気に入り店舗</h2>
+    <ul>
+      <li class="allArticles__item store-card" v-for="(article, index) in favoriteArticles" :key="index">
+        <p>{{ article.store_name }}</p>
+        <p>{{ article.store_categroy }}</p>
+        <p>{{ article.store_area }}</p>
+        <p>{{ article.store_small_text }}</p>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import firebase from '~/plugins/firebase'
+
+import { mapState, mapGetters, mapMutations, mapActions } from "vuex";
+
 export default {
   middleware: 'authenticated',
   data() {
@@ -15,9 +27,16 @@ export default {
     }
   },
   computed: {
-    
+    ...mapState('mypage', ['favoriteArticles']),
+  },
+  methods: {
+    ...mapActions('mypage', ['getFavoriteArticles']),
   },
   created: function() {
+    firebase.auth().onAuthStateChanged(user => {
+      //リロード後に実行したい処理
+      this.getFavoriteArticles(user)
+    })
   },
 }
 </script>
--- wendy/store/mypage.js
+++ wendy/store/mypage.js
@@ -1,12 +1,21 @@
 import firebase from '~/plugins/firebase'
 
+const db = firebase.firestore()
+const articlesRef = db.collection('articles')
+const usersRef = db.collection('users')
+
 export const state = () => ({
-  linePosition: ''
+  linePosition: '',
+  favoriteArticles: []
 })
 
 export const mutations = {
   setLinePositionState(state, position) {
     state.linePosition = position
+  },
+  setFavoriteArticles(state, favoriteArticles) {
+    state.favoriteArticles.splice(0)
+    state.favoriteArticles.push(...favoriteArticles)
   }
 }
 
@@ -17,6 +26,52 @@ export const actions = {
     setTimeout(() => {
       commit('setLinePositionState', position)
     }, 80);
+  },
+
+  getFavoriteArticles({ commit }, user) {
+    console.log('start');
+    console.log(user.uid);
+    // let wendyLocal = localStorage.getItem('wendy')
+    // console.log(JSON.parse(wendyLocal).auth.login_user.uid);
+
+    usersRef.doc(`${user.uid}`).collection('favorite_articles').get()
+    .then(function(querySnapshot) {
+
+      let i = querySnapshot.size
+      let favoriteArticles = [];
+      let articleCount = 0;
+
+      querySnapshot.forEach(async doc => {
+          // doc.data() is never undefined for query doc snapshots
+          console.log(doc.id, " => ", doc.data());
+
+          const addData = {
+            store_name: '',
+            store_category: '',
+            store_area: '',
+            store_small_text: ''
+          }
+
+          favoriteArticles.push(addData)
+
+          const articleSnapshot = await doc.data().ref.get();
+          favoriteArticles[articleCount].store_name = articleSnapshot.get('store_name')
+          favoriteArticles[articleCount].store_category = articleSnapshot.get('store_category')
+          favoriteArticles[articleCount].store_area = articleSnapshot.get('store_area')
+          favoriteArticles[articleCount].store_small_text = articleSnapshot.get('store_small_text')
+          articleCount++
+
+          console.log(favoriteArticles);
+
+          i--
+          if (i == 0) {
+            commit('setFavoriteArticles', favoriteArticles)
+          }  
+      });
+    })
+    .catch(function(error) {
+        console.log("Error getting documents: ", error);
+    });
   }
 }
 
