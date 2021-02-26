f2f20127632c6d672976ed88c61abb458767c83f
gawgaw <gawgaw.developer@gmail.com>
2021-02-26 20:49:53 +0900

    カメラ最終調整

--- wendy/pages/article/scan.vue
+++ wendy/pages/article/scan.vue
@@ -27,7 +27,7 @@ export default {
     return {
       result: '',
       error: '',
-      camera: 'auto'
+      camera: 'front'
     }
   },
   methods: {
@@ -67,23 +67,23 @@ export default {
     },
 
     async onInit(promise) {
-      // try {
-      //   await promise
-      // } catch (error) {
-      //   if (error.name === 'NotAllowedError') {
-      //     this.error = 'ERROR: you need to grant camera access permisson'
-      //   } else if (error.name === 'NotFoundError') {
-      //     this.error = 'ERROR: no camera on this device'
-      //   } else if (error.name === 'NotSupportedError') {
-      //     this.error = 'ERROR: secure context required (HTTPS, localhost)'
-      //   } else if (error.name === 'NotReadableError') {
-      //     this.error = 'ERROR: is the camera already in use?'
-      //   // } else if (error.name === 'OverconstrainedError') {
-      //   //   this.error = 'ERROR: installed cameras are not suitable'
-      //   } else if (error.name === 'StreamApiNotSupportedError') {
-      //     this.error = 'ERROR: Stream API is not supported in this browser'
-      //   }
-      // }
+      try {
+        await promise
+      } catch (error) {
+        if (error.name === 'NotAllowedError') {
+          this.error = 'ERROR: you need to grant camera access permisson'
+        } else if (error.name === 'NotFoundError') {
+          this.error = 'ERROR: no camera on this device'
+        } else if (error.name === 'NotSupportedError') {
+          this.error = 'ERROR: secure context required (HTTPS, localhost)'
+        } else if (error.name === 'NotReadableError') {
+          this.error = 'ERROR: is the camera already in use?'
+        } else if (error.name === 'OverconstrainedError') {
+          this.error = 'ERROR: installed cameras are not suitable'
+        } else if (error.name === 'StreamApiNotSupportedError') {
+          this.error = 'ERROR: Stream API is not supported in this browser'
+        }
+      }
     }
   }
 }
