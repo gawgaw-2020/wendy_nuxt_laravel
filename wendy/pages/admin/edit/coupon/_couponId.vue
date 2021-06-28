97dc915b36dd6a65c025d60c88da304cf44bfe90
gawgaw <gawgaw.developer@gmail.com>
2021-06-28 17:56:45 +0900

    Extension test

--- wendy/pages/admin/edit/coupon/_couponId.vue
+++ wendy/pages/admin/edit/coupon/_couponId.vue
@@ -2,7 +2,7 @@
   <div class="coupon-edit">
     <div class="coupon-input">
       <div class="coupon-input__inner">
-        <AdminPageTitle :page-title="'クーポン情報編集'"/>
+        <AdminPageTitle :page-title="'クーポン情報編集'" />
         <div class="input">
           <div v-if="coupon.category === '遅ランチ'">
             <div class="user-input">
@@ -372,11 +372,19 @@
           </div>
           <div class="user-input">
             <label for="title">お得になる商品・コース名</label>
-            <p class="user-input__input"><input id="title" type="text" v-model="coupon.title"></p>
+            <p class="user-input__input">
+              <input id="title" type="text" v-model="coupon.title" />
+            </p>
           </div>
           <div class="user-input">
             <label for="normal_price">通常価格</label>
-            <p class="user-input__input"><input id="normal_price" type="text" v-model="coupon.normal_price"></p>
+            <p class="user-input__input">
+              <input
+                id="normal_price"
+                type="text"
+                v-model="coupon.normal_price"
+              />
+            </p>
           </div>
           <div class="user-input">
             <label for="discount_rate">割引率</label>
@@ -399,45 +407,100 @@
           </div>
           <div class="user-input">
             <p>割引後価格※自動計算されます</p>
-            <p class="user-input__input">{{ Math.floor(coupon.normal_price * ((100 - coupon.discount_rate) / 100)).toLocaleString() }}円</p>
+            <p class="user-input__input">
+              {{
+                Math.floor(
+                  coupon.normal_price * ((100 - coupon.discount_rate) / 100)
+                ).toLocaleString()
+              }}円
+            </p>
           </div>
           <div class="user-input">
             <label for="how_to_use">利用方法</label>
-            <p class="user-input__input"><input id="how_to_use" type="text" v-model="coupon.how_to_use"></p>
+            <p class="user-input__input">
+              <input id="how_to_use" type="text" v-model="coupon.how_to_use" />
+            </p>
           </div>
           <div class="user-input">
             <label for="rule">利用条件</label>
-            <p class="user-input__input"><input id="rule" type="text" v-model="coupon.rule"></p>
+            <p class="user-input__input">
+              <input id="rule" type="text" v-model="coupon.rule" />
+            </p>
           </div>
           <div class="user-input">
             <label for="information">フリーコメント</label>
-            <textarea id="title" cols="30" rows="10" v-model="coupon.information"></textarea>
+            <textarea
+              id="title"
+              cols="30"
+              rows="10"
+              v-model="coupon.information"
+            ></textarea>
           </div>
-          <button class="btn btn-primary" @click="update">この内容で登録する</button>
+          <button class="btn btn-primary" @click="update">
+            この内容で登録する
+          </button>
         </div>
       </div>
     </div>
     <div class="coupon-content">
       <div class="coupon-content__inner">
-        <AdminPageTitle :page-title="'現在編集中のクーポン'"/>
+        <AdminPageTitle :page-title="'現在編集中のクーポン'" />
         <div class="coupon">
           <div class="coupon__header">
             <div class="coupon__image">
               <div class="coupon__tag">
                 <p class="coupon__category">{{ coupon.category }}</p>
-                <p class="coupon__time">{{ coupon.start }}~{{ coupon.end }}の入店限定</p>
+                <p class="coupon__time">
+                  {{ coupon.start }}~{{ coupon.end }}の入店限定
+                </p>
               </div>
-              <img :src="coupon.image" alt="">
-              <p class="coupon__badge"><img :src="`/img/discount-badge/discount-badge${coupon.discount_rate}.png`" alt=""></p>
+              <img :src="coupon.image" alt="" />
+              <p class="coupon__badge">
+                <img
+                  :src="
+                    `/img/discount-badge/discount-badge${coupon.discount_rate}.png`
+                  "
+                  alt=""
+                />
+              </p>
             </div>
           </div>
-          <div  class="coupon__content" :style="{ backgroundImage: 'url(/img/' + coupon.coupon_id + '-bg.png)' }">
+          <div
+            class="coupon__content"
+            :style="{
+              backgroundImage: 'url(/img/' + coupon.coupon_id + '-bg.png)'
+            }"
+          >
             <p class="coupon__title">{{ coupon.title }}</p>
-            <p v-if="coupon.discount_rate === 0" class="coupon__discount-rate"><span :class="coupon.coupon_id">{{ coupon.category }}で特別価格</span></p>
-            <div v-else class="coupon__discount-rate"><p><span :class="coupon.coupon_id">{{ coupon.category }}で{{ coupon.discount_rate }}%OFF</span></p></div>
-            <p v-if="coupon.discount_rate === 0" class="coupon__normal-price-blank"></p>
-            <p v-else class="coupon__normal-price">通常 {{ coupon.normal_price.toLocaleString() }}円</p>
-            <p class="coupon__discount-price">ずらし飯価格 <span>{{ Math.floor(coupon.normal_price * ((100 - coupon.discount_rate) / 100)).toLocaleString() }}<span class="yen">円</span></span></p>
+            <p v-if="coupon.discount_rate === 0" class="coupon__discount-rate">
+              <span :class="coupon.coupon_id"
+                >{{ coupon.category }}で特別価格</span
+              >
+            </p>
+            <div v-else class="coupon__discount-rate">
+              <p>
+                <span :class="coupon.coupon_id"
+                  >{{ coupon.category }}で{{ coupon.discount_rate }}%OFF</span
+                >
+              </p>
+            </div>
+            <p
+              v-if="coupon.discount_rate === 0"
+              class="coupon__normal-price-blank"
+            ></p>
+            <p v-else class="coupon__normal-price">
+              通常 {{ coupon.normal_price.toLocaleString() }}円
+            </p>
+            <p class="coupon__discount-price">
+              ずらし飯価格
+              <span
+                >{{
+                  Math.floor(
+                    coupon.normal_price * ((100 - coupon.discount_rate) / 100)
+                  ).toLocaleString()
+                }}<span class="yen">円</span></span
+              >
+            </p>
           </div>
           <dl class="coupon__infotext">
             <dt>利用方法</dt>
@@ -446,7 +509,12 @@
             <dd>{{ coupon.rule }}</dd>
             <dt>フリーコメント</dt>
             <dd>
-              <p v-for="(str, index) in (coupon.information).split('\n')" :key="index">{{ str }}</p>
+              <p
+                v-for="(str, index) in coupon.information.split('\n')"
+                :key="index"
+              >
+                {{ str }}
+              </p>
             </dd>
           </dl>
         </div>
@@ -456,75 +524,74 @@
 </template>
 
 <script>
-import firebase from 'firebase/app'
-import 'firebase/firestore'
-import "firebase/auth"
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 //firebaseのDBを定義する
-const db = firebase.firestore()
-const suspendedArticlesRef = db.collection('suspended_articles')
-const articlesRef = db.collection('articles')
+const db = firebase.firestore();
+const suspendedArticlesRef = db.collection("suspended_articles");
+const articlesRef = db.collection("articles");
 
 import { mapState, mapGetters, mapMutations, mapActions } from "vuex";
-
 
 export default {
   head() {
     return {
-      title: 'クーポン編集画面'
-    }
+      title: "クーポン編集画面"
+    };
   },
-  middleware: 'storeAuthenticated',
-  layout: 'admin',
+  middleware: "storeAuthenticated",
+  layout: "admin",
   data() {
     return {
       coupon: {}
-    }
+    };
   },
   async asyncData(context) {
     let coupon;
     let coupon_id;
 
-    let wendyLocal = localStorage.getItem('wendy')
-    const user = JSON.parse(wendyLocal)['store-auth'].login_store
+    let wendyLocal = localStorage.getItem("wendy");
+    const user = JSON.parse(wendyLocal)["store-auth"].login_store;
 
-    await suspendedArticlesRef.doc(user.uid).get()
-    .then(async function(doc) {
-      if(doc.exists) {
-        await suspendedArticlesRef
-          .doc(user.uid)
-          .collection("coupons")
-          .doc(context.params.couponId)
-          .get()
-          .then(function (doc) {
-            coupon_id = doc.id
-            coupon = {coupon_id, ...doc.data()}
-          });
-      } else {
-        await articlesRef
-          .doc(user.uid)
-          .collection("coupons")
-          .doc(context.params.couponId)
-          .get()
-          .then(function (doc) {
-            coupon_id = doc.id
-            coupon = {coupon_id, ...doc.data()}
-          });
-      }
-    })
-    return { coupon }
+    await suspendedArticlesRef
+      .doc(user.uid)
+      .get()
+      .then(async function(doc) {
+        if (doc.exists) {
+          await suspendedArticlesRef
+            .doc(user.uid)
+            .collection("coupons")
+            .doc(context.params.couponId)
+            .get()
+            .then(function(doc) {
+              coupon_id = doc.id;
+              coupon = { coupon_id, ...doc.data() };
+            });
+        } else {
+          await articlesRef
+            .doc(user.uid)
+            .collection("coupons")
+            .doc(context.params.couponId)
+            .get()
+            .then(function(doc) {
+              coupon_id = doc.id;
+              coupon = { coupon_id, ...doc.data() };
+            });
+        }
+      });
+    return { coupon };
   },
-  computed: {
-  },
+  computed: {},
   methods: {
     ...mapActions("store-edit", ["updateCoupon"]),
     update() {
-      this.updateCoupon(this.coupon)
+      this.updateCoupon(this.coupon);
     }
   },
-  created: function() {
-  },
-}
+  created: function() {}
+};
 </script>
 
 <style lang="scss" scoped>
@@ -534,12 +601,13 @@ export default {
     display: flex;
   }
 }
-.coupon-content, .coupon-input {
+.coupon-content,
+.coupon-input {
   &__inner {
     width: 90%;
     margin: 0 auto;
   }
-    @include mq(lg) {
+  @include mq(lg) {
     width: 50%;
   }
 }
@@ -577,13 +645,13 @@ export default {
     color: #ff427a;
     display: inline-block;
     margin-right: 0.8rem;
-    text-shadow:0 0 5px #888;
+    text-shadow: 0 0 5px #888;
   }
   &__time {
     font-size: 1.2rem;
     color: #fff;
     display: inline-block;
-    text-shadow:0 0 5px #000;
+    text-shadow: 0 0 5px #000;
   }
   &__badge {
     width: 80px;
@@ -670,6 +738,4 @@ export default {
     }
   }
 }
-
-
 </style>
